dec3e265f5d6270b52e8d4592e03f0228f6cb038
Murphy-hub <murphy.bela@gmail.com>
2023-03-06 11:00:51 +0800

    test: license

---
+++ test/ws.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"seehuhn.de/go/websocket"
+	"testing"
+)
+
+func TestDecimal(t *testing.T) {
+	_ = &websocket.Handler{
+		Handle: func(conn *websocket.Conn) {
+
+		},
+	}
+}
